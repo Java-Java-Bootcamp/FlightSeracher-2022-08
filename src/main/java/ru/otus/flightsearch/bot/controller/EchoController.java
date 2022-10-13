54da6ce84a0b9ad6ff952a630a9fee7741ebba2b
Timone5 <khalimovtimur95@gmail.com>
2022-10-13 16:53:50 +0300

    002 Создал spring boot проект для телеграмм бота

---
+++ src/main/java/ru/otus/flightsearch/bot/controller/EchoController.java
@@ -0,0 +1,13 @@
+package ru.otus.flightsearch.bot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EchoController {
+    @GetMapping("/echo")
+    public String echo(@RequestParam("s") String s) {
+        return s;
+    }
+}
